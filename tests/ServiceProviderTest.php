67fc0f4c0cb92c1a5dcd8b7f2f8a7fe6b0c6cc17
Marcus Bointon <marcus@synchromedia.co.uk>
2023-04-12 10:09:23 +0200

    Change test namespace

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace AltThree\Tests\Segment;
+namespace Synchro\Segment\Tests;
 
 use AltThree\Segment\SegmentServiceProvider;
 use GrahamCampbell\TestBench\AbstractPackageTestCase;
